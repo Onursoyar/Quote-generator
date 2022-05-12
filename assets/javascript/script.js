b311c49d00715822bef21ba7e808bfc8b48de8fb
Onursoyar <soyaronur@gmail.com>
2022-05-12 02:47:07 +0000

    Last toches and updates

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,23 +3,27 @@ const author = document.getElementById('quotee');
 
 const generateNew = async () => {
 
+    // The below code is where the quotes are generated from through API
     var url = "https://type.fit/api/quotes";
 
+    // The below code is needed to fetch the API
     const response = await fetch(url);
+    //Converts code in JSON
     console.log(typeof response);
 
     const allQuotes = await response.json();
 
+    //To generate random number for quotes from 0 until the length of quotes
     const indx = Math.floor(Math.random() * allQuotes.length);
-
+    //Random generated index stores the quote
     const quotes = allQuotes[indx].text;
-
+    //Random generated index stores the quotee
     const originator = allQuotes[indx].author;
 
     if (originator == null) {
         author = "Unknown";
     }
-
+    //The function to display the data taken from API
     text.innerHTML = quotes;
     author.innerHTML = '-' + originator;
 
